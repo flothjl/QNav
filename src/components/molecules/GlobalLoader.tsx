9a37e3371d127922979afa3d0b11ee261cb45763
Josh Floth <flothjldev@proton.me>
2023-08-02 21:23:39 -0500

    little more cleanup on styling of loader

--- src/components/molecules/GlobalLoader.tsx
+++ src/components/molecules/GlobalLoader.tsx
@@ -3,9 +3,19 @@ import QNavContext from "@contexts/QNavContext";
 
 const GlobalLoader: React.FC<PropsWithChildren> = ({ children }) => {
   const qNav = React.useContext(QNavContext);
-  
-  return (qNav.isLoading || !qNav) ? <p>Loading!</p> : <>{children}</>
-};
 
+  if (qNav.isLoading || !qNav) {
+    return (
+      <div className="flex h-full flex-col items-center justify-center">
+        <div className="h-full w-full px-3 py-3 text-center">
+          <p className="text-md">
+            Loading...
+          </p>
+        </div>
+      </div>
+    );
+  }
+  return <>{children}</>
+};
 
 export default GlobalLoader;
