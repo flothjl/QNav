cb5e15a1447338dabd85406a5540a801ec512ea3
Josh Floth <flothjldev@proton.me>
2023-09-05 21:42:29 -0500

    cleanup

--- src/contexts/QNavContext.tsx
+++ src/contexts/QNavContext.tsx
@@ -1,31 +1,6 @@
 import { createContext } from "react";
 import { QNavHook } from "../types";
 
-const QNavContext = createContext<QNavHook>({
-  web5: null,
-  did: null,
-  isLoading: true,
-  error: null,
-  links: [],
-  follows: [],
-  addLink: async () => {
-    return false;
-  },
-  queryLinks: async () => {
-    return false;
-  },
-  deleteLink: async () => {
-    return false;
-  },
-  addFollow: async () => {
-    return false;
-  },
-  queryFollows: async () => {
-    return false;
-  },
-  deleteFollow: async () => {
-    return false;
-  },
-});
+const QNavContext = createContext<QNavHook>({} as QNavHook);
 
 export default QNavContext;
